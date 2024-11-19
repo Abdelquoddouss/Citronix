ec59b3b141b5729dac43553cda626879afc6e8cc
Abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-11-19 22:17:09 +0100

    valider  la somme des superficies des champs est strictement inférieure à celle de la ferme

--- src/main/java/com/java/citronix/service/Impl/ChampServiceImpl.java
+++ src/main/java/com/java/citronix/service/Impl/ChampServiceImpl.java
@@ -3,6 +3,7 @@ package com.java.citronix.service.Impl;
 import com.java.citronix.domaine.entities.Champ;
 import com.java.citronix.domaine.entities.Ferme;
 import com.java.citronix.exception.ResourceNotFoundException;
+import com.java.citronix.exception.SuperficieValidationException;
 import com.java.citronix.repository.ChampRepository;
 import com.java.citronix.repository.FermeRepository;
 import com.java.citronix.service.ChampService;
@@ -26,20 +27,43 @@ public class ChampServiceImpl implements ChampService {
     public Champ save(Champ champ, UUID fermeId) {
         Ferme ferme = fermeRepository.findById(fermeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Ferme not found with ID: " + fermeId));
+
+        double totalSuperficie = ferme.getChamps().stream()
+                .mapToDouble(Champ::getSuperficie)
+                .sum();
+
+        totalSuperficie += champ.getSuperficie();
+
+        if (totalSuperficie >= ferme.getSuperficie()) {
+            throw new SuperficieValidationException("The total area of the fields exceeds the farm's available area.");
+        }
+
         champ.setFerme(ferme);
         return champRepository.save(champ);
     }
 
+
     @Override
     public Champ updateChamp(UUID champId, Champ champDetails) {
-        return champRepository.findById(champId)
-                .map(existingChamp -> {
-                    existingChamp.setSuperficie(champDetails.getSuperficie());
-                    // Mettre à jour d'autres champs si nécessaire
-                    return champRepository.save(existingChamp);
-                })
+        Champ existingChamp = champRepository.findById(champId)
                 .orElseThrow(() -> new ResourceNotFoundException("Champ not found with ID: " + champId));
+
+        Ferme ferme = existingChamp.getFerme();
+
+        double totalSuperficie = ferme.getChamps().stream()
+                .filter(champ -> !champ.getId().equals(champId))
+                .mapToDouble(Champ::getSuperficie)
+                .sum();
+
+        totalSuperficie += champDetails.getSuperficie();
+
+        if (totalSuperficie >= ferme.getSuperficie()) {
+            throw new SuperficieValidationException("The total area of the fields exceeds the farm's available area.");
+        }
+        existingChamp.setSuperficie(champDetails.getSuperficie());
+        return champRepository.save(existingChamp);
     }
+
 
     @Override
     public Champ getChampById(UUID champId) {
